fc09b8a49e537500856f1dae41203cc31be2789d
Saad Ali <engr.saadali786@gmail.com>
2024-12-22 16:34:10 +0500

    Import all old pelican config

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,21 @@
+# Site Author Information
 AUTHOR = 'Saad Ali'
+
+# Site General Information
 SITENAME = 'NIXKNIGHT'
 SITESUBTITLE = 'Not just another NIX admin'
-SITEURL = ""
 
+# Content Path
 PATH = "content"
 
-TIMEZONE = 'Asia/Karachi'
+# Plugins Configuration
+PLUGINS = [ 'sitemap' ]
 
+# Theme Settings
+THEME = 'Dark-Theme'
+
+# Localization Settings
+TIMEZONE = 'Asia/Karachi'
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
@@ -16,21 +25,58 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
+# URL and Save As Configuration for Authors and Categories
+AUTHOR_URL = False
+AUTHOR_SAVE_AS = False
+CATEGORY_URL = False
+CATEGORY_SAVE_AS = False
+
+# URL and Save As Configuration for Articles, Tags, and Pages
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}/index.html'
+ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}/'
+TAGS_SAVE_AS = 'tags/index.html'
+TAG_URL = 'tags/{slug}/'
+TAG_SAVE_AS = 'tags/{slug}/index.html'
+PAGE_URL = '{slug}/'
+PAGE_SAVE_AS = '{slug}/index.html'
+DIRECT_TEMPLATES = [ 'index', 'tags' ]
+
+# Markdown Extensions
+MARKDOWN = {
+  'extension_configs': {
+    'markdown.extensions.codehilite': {'css_class': 'highlight'},
+      'markdown.extensions.extra': {},
+      'markdown.extensions.toc': {},
+      'markdown.extensions.meta': {},
+  },
+  'output_format': 'html5',
+}
 
 # Social widget
 SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
+  ('twitter', 'https://twitter.com/SaadKnight'),
+  ('linkedin', 'https://www.linkedin.com/in/saadali27/'),
+  ('github', 'https://github.com/NIXKnight'),
 )
 
+# Pagination configuration
 DEFAULT_PAGINATION = 10
+PAGINATION_PATTERNS = (
+  (1, '{base_name}/', '{base_name}/index.html'),
+  (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+)
 
-# Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+# Sitemap Configuration
+SITEMAP = {
+  "format": "xml",
+  'priorities': {
+    'indexes': 1.0,
+    'articles': 0.8,
+    'pages': 0.5,
+  },
+  'changefreqs': {
+    'indexes': 'daily',
+    'articles': 'weekly',
+    'pages': 'monthly',
+  }
+}
